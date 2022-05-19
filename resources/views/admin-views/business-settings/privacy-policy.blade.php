55b1db6a14bfd7d2f69e589a2e325898d7be4153
dhaydt <1992dayat@gmail.com>
2022-05-19 11:31:35 +0700

    fix ckeditor vendor

--- resources/views/admin-views/business-settings/privacy-policy.blade.php
+++ resources/views/admin-views/business-settings/privacy-policy.blade.php
@@ -54,6 +54,7 @@
     {{--ck editor--}}
     <script src="{{asset('/')}}vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
     <script src="{{asset('/')}}vendor/unisharp/laravel-ckeditor/adapters/jquery.js"></script>
+
     <script>
         $('#editor').ckeditor({
             contentsLangDirection : '{{Session::get('direction')}}',
