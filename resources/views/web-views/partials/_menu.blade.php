97b0f0b86161a2f6b98365cedd2bc453eea35d46
dhaydt <1992dayat@gmail.com>
2022-06-30 08:21:37 +0700

    fix menu service name & style

--- resources/views/web-views/partials/_menu.blade.php
+++ resources/views/web-views/partials/_menu.blade.php
@@ -1,3 +1,17 @@
+@push('css_or_js')
+    <style>
+        .route-menu{
+            text-decoration: none;
+        }
+        .route-menu p{
+            font-weight: 700;
+            transition: 0.3s;
+        }
+        .route-menu:hover p{
+            color: {{ $web_config['secondary_color'] }};
+        }
+    </style>
+@endpush
 {{-- @foreach($categories as $category)
 <div class="category_div" style="height: 132px; width: 100%;">
     <a href="{{route('products',['id'=> $category['id'],'data_from'=>'category','page'=>1])}}">
@@ -14,28 +28,28 @@
         <a href="javascript:" class="route-menu">
             <img style="vertical-align: middle; padding: 16%;height: 98px"
                 onerror="this.src='{{asset('assets/front-end/img/image-place-holder.png')}}'" src="{{ asset('assets/front-end/img/wahl.png') }}" alt="Wahl">
-            <p class="text-center small" style="margin-top: -20px">OnCukur</p>
+            <p class="text-center small" style="margin-top: -15px">Booking cukur</p>
         </a>
     </div>
     <div class="category_div">
         <a href="{{ route('onlocation') }}" class="route-menu">
             <img style="vertical-align: middle; padding: 16%;height: 98px"
                 onerror="this.src='{{asset('assets/front-end/img/image-place-holder.png')}}'" src="{{ asset('assets/front-end/img/map.png') }}" alt="Map">
-            <p class="text-center small" style="margin-top: -20px">OnLokasi</p>
+            <p class="text-center small" style="margin-top: -15px">Order visit</p>
         </a>
     </div>
     <div class="category_div">
         <a href="javascript:" class="route-menu">
             <img style="vertical-align: middle; padding: 16%;height: 98px"
                 onerror="this.src='{{asset('assets/front-end/img/image-place-holder.png')}}'" src="{{ asset('assets/front-end/img/hair.png') }}" alt="Style">
-            <p class="text-center small" style="margin-top: -20px">Style Kamu</p>
+            <p class="text-center small" style="margin-top: -15px">Style Kamu</p>
         </a>
     </div>
     <div class="category_div">
         <a href="javascript:" class="route-menu">
             <img style="vertical-align: middle; padding: 16%;height: 98px"
                 onerror="this.src='{{asset('assets/front-end/img/image-place-holder.png')}}'" src="{{ asset('assets/front-end/img/other.png') }}" alt="other">
-            <p class="text-center small" style="margin-top: -20px">Lain - Lain</p>
+            <p class="text-center small" style="margin-top: -15px">Lain - Lain</p>
         </a>
     </div>
 </div>
